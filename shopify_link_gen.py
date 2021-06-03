df434f5ce120079ca9c2a1e8ff7ddd6e955e2c0f
Nils <nils-doering@gmx.de>
2021-06-03 14:45:33 +0200

    Update shopify_link_gen.py

--- shopify_link_gen.py
+++ shopify_link_gen.py
@@ -1,3 +1,7 @@
+from urllib.request import urlopen
+import requests
+import xml.etree.ElementTree as ET
+import urllib
 
 #asking for shopify link
 print("What is the Web Adress of the product? e.g.: https://eu.oneblockdown.it/products/ambush-dunk (Please with https:// infront)")
@@ -5,10 +9,22 @@ print("What is the Web Adress of the product? e.g.: https://eu.oneblockdown.it/p
 #getting shopify link
 link = input()
 print(link)
-
+url = link +'.xml'
+print(url)
+response = urllib.request.urlopen(url).read()
 #asking for output
 print("Do you want the output in Discord Embed (1) or just normal the links (2)?")
 
 #getting output info
 output = input()
 print(output)
+
+#parsing
+tree = ET.fromstring(response)
+root = tree.getroot()
+
+for sizecat in root.findall('variant'):
+    id = sizecat.find('id').text
+    size = sizecat.find('option1').text
+
+    print(id, size)
