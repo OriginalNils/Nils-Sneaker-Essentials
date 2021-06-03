950512dc68c4ab1a9b9d585411af88ef48177fdb
Nils <nils-doering@gmx.de>
2021-06-03 13:06:43 +0200

    Create shopify_link_gen.py

---
+++ shopify_link_gen.py
@@ -0,0 +1,14 @@
+
+#asking for shopify link
+print("What is the Web Adress of the product? e.g.: https://eu.oneblockdown.it/products/ambush-dunk (Please with https:// infront)")
+
+#getting shopify link
+link = input()
+print(link)
+
+#asking for output
+print("Do you want the output in Discord Embed (1) or just normal the links (2)?")
+
+#getting output info
+output = input()
+print(output)
