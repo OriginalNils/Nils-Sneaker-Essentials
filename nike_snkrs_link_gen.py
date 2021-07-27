f083c3c4ac15f33bd83fffdafabe9c45225fdf12
Nils <45696928+OriginalNils@users.noreply.github.com>
2021-07-27 10:45:57 +0200

    Update nike_snkrs_link_gen.py

--- nike_snkrs_link_gen.py
+++ nike_snkrs_link_gen.py
@@ -2,7 +2,7 @@ import urllib.request
 import requests
 
 #Getting Shopify Product URL
-print("What is the Shopify products URL? (Please with https:// at the begining)")
+print("What is the Nike SNKRS products URL? (Please with https:// at the begining)")
 url = input() 
 print("------------------------")
 
@@ -10,6 +10,11 @@ print("------------------------")
 print("Please input the size You want. (In US Size e.g.: 9.5)")
 size = input()
 print("------------------------")
+
+website = urlparse(url)
+codelink = 'view-source:' + url
+print('Processing...')
+
 
 req = requests.get(url, 'html.parser')
 
